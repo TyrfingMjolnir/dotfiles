c2bdfc66bcb961a9e4803e65d930ba954f62d802
Tyrfing Mjølner <gjermund@developer.no>
2024-12-16 23:23:28 +0100

    Update telescope.multi-ripgrep.lua
    Based off of: https://youtu.be/xdXE1tOT-qg

--- .config/nvim/lua/plugin/telescope.multi-ripgrep.lua
+++ .config/nvim/lua/plugin/telescope.multi-ripgrep.lua
@@ -1,74 +1,54 @@
-local conf = require("telescope.config").values
+local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local make_entry = require "telescope.make_entry"
-local pickers = require "telescope.pickers"
+local conf = require "telescope.config".values
 
-local flatten = vim.tbl_flatten
+local M = {}
 
--- i would like to be able to do telescope
--- and have telescope do some filtering on files and some grepping
+local live_multigrep = function(opts)
+	opts = opts or {}
+	opts.cwd = opts.cwd or vim.uv.cwd()
 
-return function(opts)
-  opts = opts or {}
-  opts.cwd = opts.cwd and vim.fn.expand(opts.cwd) or vim.loop.cwd()
-  opts.shortcuts = opts.shortcuts
-    or {
-      ["l"] = "*.lua",
-      ["v"] = "*.vim",
-      ["n"] = "*.{vim,lua}",
-      ["c"] = "*.c",
-      ["p"] = "*.{py,pyc}",
-      ["r"] = "*.rs",
-      ["g"] = "*.go",
-      ["s"] = "*.swift",
-      ["cd"] = "*.xcdatamodel", -- CoreData
-      ["xib"] = "*.{xib,nib,storyboard}", -- Apple GUI
-    }
-  opts.pattern = opts.pattern or "%s"
+	local finder = finders.new_async_job {
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
 
-  local custom_grep = finders.new_async_job {
-    command_generator = function(prompt)
-      if not prompt or prompt == "" then
-        return nil
-      end
+		local pieces vim.split(prompt, "  ")
+			local args = { "rg" }
+			if pieces[1] then
+				table.insert(args, "-e")
+				table.insert(args, pieces[1])
+			end
 
-      local prompt_split = vim.split(prompt, "  ")
+			if pieces[2] then
+				table.insert(args, "-g")
+				table.insert(args, pieces[2])
+			end
 
-      local args = { "rg" }
-      if prompt_split[1] then
-        table.insert(args, "-e")
-        table.insert(args, prompt_split[1])
-      end
+			---@diagnostic disable-next-line: deprecated
+			return vim.tbl_flatten {
+				args,
+				{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+			}
+		end,
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd,
+	}
 
-      if prompt_split[2] then
-        table.insert(args, "-g")
-
-        local pattern
-        if opts.shortcuts[prompt_split[2]] then
-          pattern = opts.shortcuts[prompt_split[2]]
-        else
-          pattern = prompt_split[2]
-        end
-
-        table.insert(args, string.format(opts.pattern, pattern))
-      end
-
-      return flatten {
-        args,
-        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
-      }
-    end,
-    entry_maker = make_entry.gen_from_vimgrep(opts),
-    cwd = opts.cwd,
-  }
-
-  pickers
-    .new(opts, {
-      debounce = 100,
-      prompt_title = "Live Grep (with shortcuts)",
-      finder = custom_grep,
-      previewer = conf.grep_previewer(opts),
-      sorter = require("telescope.sorters").empty(),
-    })
-    :find()
+	pickers.new(opts, {
+		debounce = 100,
+		prompt_title = "Live multigrep",
+		finder = finder,
+		previewer = conf.grep_previewer(opts),
+		sorter = require("telescope.sorters").empty(),
+	}):find()
 end
+
+M.setup = function()
+	live_multigrep()
+end
+--	live_multigrep()
+
+return M
